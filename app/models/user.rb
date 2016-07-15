b44f657bab3d9e0bd36953fd05540df6578da30e
Desmond Poon <Desmond@Desmonds-MacBook-Pro.local>
2016-07-14 18:52:36 -0700

    User signups, logins, file uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :accounts
   has_many :storages
+
   validates :name, presence: true
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_hash, presence: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end 
